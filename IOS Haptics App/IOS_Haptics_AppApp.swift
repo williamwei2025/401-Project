1bcf769d284ce96096b2fcd6e2df2fdbe217840d
William Wei <weiwilli@usc.edu>
2022-09-24 01:53:12 -0700

    commit 1

---
+++ IOS Haptics App/IOS_Haptics_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Haptics_AppApp.swift
+//  IOS Haptics App
+//
+//  Created by user228516 on 9/24/22.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Haptics_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IOS Haptics AppUITests/IOS_Haptics_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  IOS_Haptics_AppUITestsLaunchTests.swift
+//  IOS Haptics AppUITests
+//
+//  Created by user228516 on 9/24/22.
+//
+
+import XCTest
+
+final class IOS_Haptics_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
